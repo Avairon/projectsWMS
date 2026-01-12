dca34bf6abf098b0dac6f0c58c54182c0c1035cc
Avairon <75504999+Avairon@users.noreply.github.com>
2026-01-13 03:32:39 +0700

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.utils import init_database
-
-app = create_app()
-
-if __name__ == '__main__':
-    init_database()
-    app.run(host='0.0.0.0', port=5000, debug=True)
